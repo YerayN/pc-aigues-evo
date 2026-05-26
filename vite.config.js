2fe0be0972483566b034291672024c0d883c8f55
Yeray <yeraynavarroyanini@gmail.com>
2026-05-26 09:38:27 +0200

    Implementada función de instalar app

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           { src: 'escudo.png', sizes: '192x192', type: 'image/png' },
-          { src: 'escudo.png', sizes: '512x512', type: 'image/png' }
+          { src: 'escudo.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       },
       workbox: {
